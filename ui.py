6e4255e9cde6fc6c77681ab69ae272789fa89eb1
REMY348 <104235779+REMY348@users.noreply.github.com>
2023-07-10 00:58:02 +0800

    Update ui.py
    ui baharu

--- ui.py
+++ ui.py
@@ -6,11 +6,17 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 import pygame
+import tkinter as tk
+from tkinter import filedialog, Text, Label
+import tkinter.font as tkFont
+from PIL import ImageTk, Image
+
+
 
 
 
 pygame.mixer.init()
-new_model = tf.keras.models.load_model('saved_model/my_model')
+new_model = tf.keras.models.load_model('saved_model\my_model')
 
 
 
@@ -20,23 +26,38 @@ img_width = 300
 class_names = ['broadbill', 'eagle', 'owl', 'parrot']
 root = tk.Tk()
 root.title('PROJECT BIRDS RECOGNITION!')
-root.geometry("700x650")
+root.geometry("800x600")
 filename ="null"
 apps = []
+
+
+
 def add_App():
-    global filename
-    filename = filedialog.askopenfilename(initialdir="/",title="Select File",
-    filetypes= (("all files","*.*"),("exe","*.exe")))
+    global filename, Bird_image
+    
+    # Prompt the user to select a file
+    filename = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                          filetypes=(("all files", "*.*"), ("exe", "*.exe")))
     apps.append(filename)
-    for app in apps:
-        label = tk.Label(frame,text=app,bg="white" ) 
-        label.pack() 
-    return filename  
+    
+    # Clear the previous image label
+    labelproject.configure(image=None)
+    
+    # Display the selected image
+    img = Image.open(filename)
+    img = img.resize((300, 250))
+    Bird_image = ImageTk.PhotoImage(img)
+    labelproject.configure(image=Bird_image)
+    
+    return filename
+
+
 
 
 
 def run_App():
     global filename
+    global labeloutput
     img = keras.preprocessing.image.load_img(
     filename, target_size=(img_height, img_width))
     img_array = keras.preprocessing.image.img_to_array(img)
@@ -46,36 +67,49 @@ def run_App():
     Output = (
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score)))
-    labeloutput = tk.Label(frame,text=Output,bg="white")
+    labeloutput = tk.Label(text=Output,bg="white")
     labeloutput.pack()
     
+def clear_App():
+    apps.clear()
+    labeloutput.configure(text="")
+    
+        
+background_image = ImageTk.PhotoImage(Image.open("background_image.png"))
+background_label = tk.Label(root, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
 
 
-canvas = tk.Canvas(root,height=650,width=650,bg="#63ad44")
-
-canvas.pack()
-
-frame =tk.Frame(root,bg="white")
-frame.place(relwidth=0.8,relheight=0.6,relx=0.1,rely=0.1)
 
 
 
-Fish_image =ImageTk.PhotoImage(Image.open('bird.jpg'))
 
-fontStyle = tkFont.Font(family="Poppins",size=20)
-labeltitle = Label(frame,text="PROJECT BIRD DETECTION" ,font= fontStyle,bg="white")
+
+Bird_image =ImageTk.PhotoImage(Image.open('bird.png'))
+
+fontStyle = tkFont.Font(family="Poppins",size=20,weight="bold")
+labeltitle = Label(text="PROJECT BIRD DETECTION" ,font= fontStyle)
 labeltitle.pack()
 
-labelproject = Label(frame ,image=Fish_image)
+labelproject = Label(image=Bird_image)
 labelproject.pack()
 
-line = tk.Frame(frame, height=1, width=550, bg="grey80", relief='groove')
+line = tk.Frame( height=1, width=550, bg="grey80", relief='groove')
 line.pack()
 
-openFile = tk.Button(frame,text="Open File",padx=10,pady=5,fg="white",bg="#29592f",  command=add_App  )
+openFile = tk.Button(text="Open File",padx=10,pady=5,fg="white",bg="#808080",  command=add_App  )
 openFile.pack(pady=10)
-RunApp = tk.Button(frame,text="Run App ",padx=10,pady=5,fg="white",bg="#29592f",  command=run_App  )
+openFile.pack(side= 'left')
+
+RunApp = tk.Button(text="Run App ",padx=10,pady=5,fg="white",bg="#808080",  command=run_App  )
 RunApp.pack()
+RunApp.pack(side='right')
+
+clearApp = tk.Button(text="clear",padx=10,pady=5,fg="white",bg="#808080",  command=clear_App  )
+clearApp.pack(pady=10)
+
+
+
 
 
 root.mainloop()
